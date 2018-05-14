c2c7450838d0be485c72162a6f31d3b53fc31eb9
Alex Wilson <fonzmp@gmail.com>
2018-05-14 20:15:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,5 @@
 #write your code here
 
-def countdown(number)
-  n = number.to_i
-  while n > 0
-    puts "- #{n} SECOND(S)!"
-    int -= 1
-  end
-  return "HAPPY NEW YEAR!"
+def countdown
+
 end
